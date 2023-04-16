1b767f28a7e272ea48692a621047082b04668afb
zqsuu <114447818+zqsuu@users.noreply.github.com>
2023-04-16 16:38:44 -0400

    update map

--- Balance_Calculator/frontend/src/Components/MapLocation/Map.js
+++ Balance_Calculator/frontend/src/Components/MapLocation/Map.js
@@ -1,52 +1,121 @@
-import { GoogleMap, MarkerF } from '@react-google-maps/api';
+import { GoogleMap, InfoWindowF, MarkerF } from '@react-google-maps/api';
 import Geocode from "react-geocode";
+import { dateFormat } from '../../utils/dateFormat';
 import React, { useEffect,useState } from 'react';
 import { useGlobalContext } from '../../context/globalContext';
 import { InnerLayout } from '../../styles/Layouts';
 import './Map.scss';
+import IncomeItem from './IncomeItem';
 
 function Map() {
     const {incomes, expenses, getIncomes, getExpenses } = useGlobalContext()
 
+    const [position, setPosition] = useState({lat: 42.3600825, lng: -71.0588801})
+    const [data, setData] = useState(incomes)
+    const [info, setInfo] = useState('')
+    const [checked ,setChecked] = useState([true,false])
+
     useEffect(() => {
         getIncomes()
         getExpenses()
+        setPosition({lat: 42.3600825, lng: -71.0588801})
+        setData(incomes)
+        setChecked([true,false])
+        const info = {
+            title:'',
+            amount:'',
+            date:'',
+            category:'',
+            description:'',
+            type:''
+        }
+        setInfo(info)
     }, [])
 
     Geocode.setApiKey("AIzaSyB_27lWXeJoFzJPhiUVMGSNaNMXke2Mpjo");
     Geocode.setLanguage("en");
     Geocode.setLocationType("GEOMETRIC_CENTER");
 
-    const getPosition = (location) => {
-        const lo = Geocode.fromAddress(location).then((response) => {return response.results[0].geometry.location})
-        return lo
+    const itemClicked = (params) => {
+        const [ title, amount, date, category, description, type, location] = params
+        Geocode.fromAddress(location).then((response) => {setPosition(response.results[0].geometry.location)})
+        const info = {
+            title:title,
+            amount:amount,
+            date:date,
+            category:category,
+            description:description,
+            type:type
+        }
+        setInfo(info)
     }
 
-    const center = {lat: 42.3600825, lng: -71.0588801}
+    const radioClicked = (e) => {
+        if(e.target.value === 'income'){
+            setData(incomes)
+            setChecked([true,false])
+        } 
+        else{
+            setData(expenses)
+            setChecked([false,true])
+        }
+    }
+
 
     const[map, setMap] = useState(/** @type google.maps.Map */ (null));
+    const [isOpen, setOpen] = useState(false)
+    const handleToggleOpen = () => {
+        setOpen(true)
+    }
+
+    const handleToggleClose = () => {
+        setOpen(false)
+    }
 
   return (
-    <div className="map-container">
+    <div>
       <InnerLayout>
-        <div className='map'>
-            <GoogleMap center={center} zoom={15} mapContainerStyle={{width:'100%', height:'100%'}} onLoad={(map) =>setMap(map)}>
-                <MarkerF position={center} visible={true} center={center} />
-                {expenses.map((income) => {
-                    const {_id, title, amount, date, category, description, type, location} = income;
-                    return <MarkerF
-                        key ={_id}
-                        center={getPosition(location)}
-                    />
-                })}
-                {incomes.map((income) => {
-                    const {_id, title, amount, date, category, description, type, location} = income;
-                    return <MarkerF
-                        key ={_id}
-                        center={getPosition(location)}
-                    />
-                })}
-            </GoogleMap>
+        <h1>LOCATION</h1>
+        <div className="map-container">
+            <div className="incomes">
+                <div className='selectBar'>
+                    <input type="radio" class='radio-type' name="type" checked={checked[0]} value="income" onClick={e =>radioClicked(e)}/>Income
+                    <input type="radio" class='radio-type' name="type" checked={checked[1]} value="expense" onClick={e =>radioClicked(e)}/>Expense
+                    {data.map((income) => {
+                        const {_id, title, amount, date, category, description, type, location} = income;
+                        return <IncomeItem
+                            key={_id}
+                            id={_id} 
+                            title={title} 
+                            description={description} 
+                            amount={amount} 
+                            date={date} 
+                            type={type}
+                            category={category} 
+                            location={location}
+                            indicatorColor="var(--color-green)"
+                            itemClicked={itemClicked}
+                        />
+                    })}
+                </div>
+            </div>
+            <div className='map'>
+                <GoogleMap center={position} zoom={15} mapContainerStyle={{width:'100%', height:'100%'}} onLoad={(map) =>setMap(map)}>
+                    <MarkerF position={position} onClick={handleToggleOpen} />
+                    {isOpen &&
+                    <InfoWindowF position={position} onCloseClick={handleToggleClose} >
+                        <div>
+                            <p>title: {info.title}</p>
+                            <p>amount: {info.amount}</p>
+                            <p>date: {dateFormat(info.date)}</p>
+                            <p>category: {info.category}</p>
+                            <p>description: {info.description}</p>
+                            <p>type: {info.type}</p>
+                        </div>
+                    </InfoWindowF>
+                    }
+                </GoogleMap>
+            </div>
         </div>
       </InnerLayout>
     </div>
