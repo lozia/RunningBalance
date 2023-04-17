dab62714a41f42fc8249b75945d278058103c8b2
Xinyue Zhang <zhang.xinyue12@northeastern.edu>
2023-04-16 23:17:04 -0400

    Pie-Chart

--- Balance_Calculator/frontend/src/Components/Visualization/Visualization.js
+++ Balance_Calculator/frontend/src/Components/Visualization/Visualization.js
@@ -1,34 +1,97 @@
 import React, { useEffect, useRef } from "react";
 import { useGlobalContext } from "../../context/globalContext";
-import * as d3 from "d3";
+import { select, pie, arc, scaleOrdinal, schemeCategory10 } from "d3";
 import { InnerLayout } from "../../styles/Layouts";
 import "./Visualization.scss";
 
 const Visualization = () => {
   const { incomes, expenses } = useGlobalContext();
-  const svgRef = useRef();
+  const svgIncomeRef = useRef();
+  const svgExpenseRef = useRef();
 
   useEffect(() => {
     if (!incomes || !expenses) return;
 
-    const visualizationData = [
-      { label: "Income", value: incomes.reduce((sum, income) => sum + income.amount, 0) },
-      { label: "Expense", value: expenses.reduce((sum, expense) => sum + expense.amount, 0) },
-    ];
+    const incomeData = incomes.reduce((acc, income) => {
+      const category = income.category;
+      const amount = income.amount;
+      acc[category] = (acc[category] || 0) + amount;
+      return acc;
+    }, {});
 
-    const svg = d3.select(svgRef.current);
-    // Add your D3.js visualization code here
+    const expenseData = expenses.reduce((acc, expense) => {
+      const category = expense.category;
+      const amount = expense.amount;
+      acc[category] = (acc[category] || 0) + amount;
+      return acc;
+    }, {});
 
-  }, [incomes, expenses]);
+    const visualizationDataIncome = Object.entries(incomeData).map(([label, value]) => ({ label, value }));
+    const visualizationDataExpense = Object.entries(expenseData).map(([label, value]) => ({ label, value }));
 
-  return (
-    <div className="visualization-main">
-      <InnerLayout>
-        <h1>Income and Expense Visualization</h1>
-        <svg ref={svgRef}></svg>
-      </InnerLayout>
-    </div>
-  );
-};
+    const createPieChart = (svgRef, visualizationData, chartTitle) => {
+      const width = 400;
+      const height = 400;
+      const margin = 50;
 
-export default Visualization;
+      const radius = Math.min(width, height) / 2 - margin;
+
+      const svg = select(svgRef.current)
+        .attr("width", width)
+        .attr("height", height);
+
+      svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", 30)
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("font-weight", "bold")
+        .text(chartTitle);
+
+      const g = svg
+        .append("g")
+        .attr("transform", `translate(${width / 2}, ${height / 2 + 20})`);
+
+      const d3Pie = pie().value((d) => d.value);
+
+      const d3Arc = arc()
+        .innerRadius(0)
+        .outerRadius(radius);
+
+      const colorScale = scaleOrdinal(schemeCategory10);
+
+      g.selectAll("path")
+        .data(d3Pie(visualizationData))
+        .enter()
+        .append("path")
+        .attr("d", d3Arc)
+        .attr("fill", (d, i) => colorScale(i));
+
+      g.selectAll("text")
+        .data(d3Pie(visualizationData))
+        .enter()
+        .append("text")
+        .attr("transform", (d) => `translate(${d3Arc.centroid(d)})`)
+        .attr("dy", "0.35em")
+        .style("text-anchor", "middle")
+        .style("font-size", "12px")
+        .text((d) => `${d.data.label}: ${(d.data.value / d3Pie(visualizationData).reduce((acc, d) => acc + d.data.value, 0) * 100).toFixed(2)}%`);
+      };
+      createPieChart(svgIncomeRef, visualizationDataIncome, "Income by Category");
+      createPieChart(svgExpenseRef, visualizationDataExpense, "Expense by Category");
+  
+    }, [incomes, expenses]);
+    return (
+      <div className="visualization-main">
+        <InnerLayout>
+          <h1>Income and Expense Visualization</h1>
+          <div className="visualization-charts">
+            <svg ref={svgIncomeRef}></svg>
+            <svg ref={svgExpenseRef}></svg>
+          </div>
+        </InnerLayout>
+      </div>
+    );
+  };
+  
+  export default Visualization;
